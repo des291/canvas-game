d251354adcda58b9c24819058a9636261d270356
Des <des.grainger@proton.me>
2025-01-15 21:23:07 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+  constructor(x, y, radius, colour) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.colour = colour;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.colour;
+    c.fill();
+  }
+}
+
+const player = new Player(100, 100, 30, "blue");
+player.draw();
+console.log(player);
