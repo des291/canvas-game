bb7a67e4b7e549e2842ecc2a40b0d32120ad0db8
Des <des.grainger@proton.me>
2025-01-16 20:58:39 +0000

    created projectile class

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor(x, y, radius, colour, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.colour = colour;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.colour;
+    c.fill();
+  }
+}
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
