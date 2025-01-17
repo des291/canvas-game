b339700d553450608ab22cfe7660b65b2c8d00ae
Des <des.grainger@proton.me>
2025-01-17 21:06:33 +0000

    added functionality to fire projectiles

--- index.js
+++ index.js
@@ -35,22 +35,43 @@ class Projectile {
     c.fillStyle = this.colour;
     c.fill();
   }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
 }
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "blue");
-player.draw();
-console.log(player);
+
+const projectiles = [];
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+}
 
 addEventListener("click", (event) => {
-  const projectile = new Projectile(
-    event.clientX,
-    event.clientY,
-    5,
-    "red",
-    null
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
   );
-  projectile.draw();
-  console.log(event);
+
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+  console.log(angle);
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+  );
 });
+
+animate();
